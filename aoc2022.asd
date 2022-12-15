b74941c30a66c83c800cbc8766519de36b4185c3
Johan Félisaz <johan.felisaz@protonmail.com>
2022-12-15 10:22:47 +0100

    day 13 part 2

--- aoc2022.asd
+++ aoc2022.asd
@@ -18,4 +18,5 @@
 	       (:file "day9")
 	       (:file "day10")
 	       (:file "day11")
-	       (:file "day12")))
+	       (:file "day12")
+	       (:file "day13")))
--- day13.lisp
+++ day13.lisp
@@ -1,12 +1,11 @@
 (in-package #:aoc2022)
 
 (defun d13/load-data ()
-  (with-open-file (f "data/day13") ;"data/day13"
+  (with-open-file (f "data/day13")
     (loop for l = (read-line f nil)
 	  while l
 	  unless (string= l "")
-	    collect (parse-packet l) into packets
-	  finally (return (group-by 2 packets)))))
+	    collect (parse-packet l))))
 
 (defun parse-packet (line)
   (setf line (ppcre:regex-replace-all "," line " "))
@@ -15,7 +14,6 @@
   (read-from-string line))
 
 (defun packet-compare (a b)
-  ;; (print (list a b))
   (cond ((and (numberp a) (numberp b)) (if (= a b) :unknown (< a b)))
 	((and (listp a) (listp b))
 	 (loop for x in a for y in b do
@@ -26,6 +24,14 @@
 	(t (if (numberp a) (packet-compare (list a) b) (packet-compare a (list b))))))
 
 (defun d13/1 ()
-  (loop for (a b) in (d13/load-data)
+  (loop for (a b) in (group-by 2 (d13/load-data))
 	for n from 1
-	when (packet-compare a b) collect n))
+	when (packet-compare a b) sum n))
+
+(defun d13/2 ()
+  (let* ((divider1 '((2)))
+	 (divider2 '((6)))
+	 (ordered-packets (sort (append (list divider1 divider2) (d13/load-data))
+				#'packet-compare)))
+    (* (1+ (position divider1 ordered-packets :test #'equal))
+       (1+ (position divider2 ordered-packets :test #'equal)))))
